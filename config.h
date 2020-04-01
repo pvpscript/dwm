e59101c62cf35043b89d78496f58e46a055264a3
Pedro Vitor Piassa <pvpiassa@gmail.com>
2020-04-01 20:51:18 -0300

    Changing fonts config

--- config.h
+++ config.h
@@ -12,7 +12,10 @@ static const int smartgaps          = 1;        /* 1 means no outer gap when the
 
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = {
+	"monospace:pixelsize=12:autohint=true:antialias=true",
+	"DejaVu Sans Mono:pixelsize=12:autohint=true:antialias=true"
+};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
