c447aa687faf837f4a2b09d7cb8d4231141457eb
Pedro Vitor Piassa <pvpiassa@gmail.com>
2020-04-05 23:50:47 -0300

    Changed color scheme to 'black-blueish'

--- config.h
+++ config.h
@@ -29,13 +29,33 @@ static const char col_red[]         = "#ff0000";
 static const char col_yellow[]      = "#ffff00";
 static const char col_white[]       = "#ffffff";
 
+static const char norm_fg[] = "#dbdcd1";
+static const char norm_bg[] = "#0c1220";
+static const char norm_border[] = "#999a92";
+
+static const char sel_fg[] = "#dbdcd1";
+static const char sel_bg[] = "#A0B19C";
+static const char sel_border[] = "#dbdcd1";
+
+static const char urg_fg[] = "#dbdcd1";
+static const char urg_bg[] = "#70A9A7";
+static const char urg_border[] = "#70A9A7";
+
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeWarn] = { col_black, col_yellow, col_red },
-	[SchemeUrgent] = { col_white, col_red,    col_red },
+	/*               fg           bg         border                         */
+	[SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
+	[SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win
+	[SchemeUrg] =  { urg_fg,      urg_bg,    urg_border },
 };
+
+
+// static const char *colors[][3]      = {
+// 	/*               fg         bg         border   */
+// 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+// 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+// 	[SchemeWarn] = { col_black, col_yellow, col_red },
+// 	[SchemeUrgent] = { col_white, col_red,    col_red },
+// };
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
