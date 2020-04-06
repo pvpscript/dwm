7e2e6df093c1f2e5bab2720babc12c1f1a55f625
Pedro Vitor Piassa <pvpiassa@gmail.com>
2020-04-05 23:56:06 -0300

    Fixed color scheme name

--- config.h
+++ config.h
@@ -45,7 +45,7 @@ static const char *colors[][3]      = {
 	/*               fg           bg         border                         */
 	[SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
 	[SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win
-	[SchemeUrg] =  { urg_fg,      urg_bg,    urg_border },
+	[SchemeWarn] =  { urg_fg,      urg_bg,    urg_border },
 };
 
 
